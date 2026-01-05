deaf8cdcf833687cc604f1131b73823120858fbf
Admin <kirillchistov@MacBook-Pro-Admin-2.local>
2026-01-05 19:23:20 +0300

    hw2 update readme

--- vite.config.ts
+++ vite.config.ts
@@ -19,9 +19,9 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        register: resolve(__dirname, 'register.html'),
-        messenger: resolve(__dirname, 'messenger.html')
+        main: resolve(__dirname, 'src/index.html'),
+        register: resolve(__dirname, 'src/register.html'),
+        // messenger: resolve(__dirname, 'messenger.html')
       }
     }
   },
