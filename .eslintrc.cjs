b9833788956e1f65baffbc15334d3f7e3f89cbb8
Admin <kirillchistov@MacBook-Pro-Admin-2.local>
2026-01-14 12:37:03 +0300

    Fix eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,8 +19,20 @@ module.exports = {
     'no-console': 'warn',
     'import/prefer-default-export': 'off',
 
-    // off правила, требующие parserServices
+    // off правила, требующие parserServices и др.
     '@typescript-eslint/dot-notation': 'off',
     '@typescript-eslint/no-implied-eval': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+    "@typescript-eslint/lines-between-class-members": "off",
+
+    "import/prefer-default-export": "off",
+    "class-methods-use-this": "off",
+    "no-restricted-exports": "off",
+    "no-param-reassign": "off",
+    "import/no-absolute-path": "off",
+    "consistent-return": "off",
+    "import/no-cycle": "off",
+    "prefer-promise-reject-errors": "off",
+    "no-alert": "off",
   },
 };
