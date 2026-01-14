a2fddc4c1906ec460a134f4d291895725c9d301e
Admin <kirillchistov@MacBook-Pro-Admin-2.local>
2026-01-14 12:41:32 +0300

    Fix eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,21 +18,20 @@ module.exports = {
   rules: {
     'no-console': 'warn',
     'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'no-restricted-exports': 'off',
+    'no-param-reassign': 'off',
+    'import/no-absolute-path': 'off',
+    'consistent-return': 'off',
+    'import/no-cycle': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'no-alert': 'off',
 
     // off правила, требующие parserServices и др.
     '@typescript-eslint/dot-notation': 'off',
     '@typescript-eslint/no-implied-eval': 'off',
     '@typescript-eslint/no-throw-literal': 'off',
-    "@typescript-eslint/lines-between-class-members": "off",
-
-    "import/prefer-default-export": "off",
-    "class-methods-use-this": "off",
-    "no-restricted-exports": "off",
-    "no-param-reassign": "off",
-    "import/no-absolute-path": "off",
-    "consistent-return": "off",
-    "import/no-cycle": "off",
-    "prefer-promise-reject-errors": "off",
-    "no-alert": "off",
+    '@typescript-eslint/return-await': 'off',
+    '@typescript-eslint/lines-between-class-members': 'off',
   },
 };
