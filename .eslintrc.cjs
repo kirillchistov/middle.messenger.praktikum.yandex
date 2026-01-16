b87c0a07b9b2f3d5ab8cbdafcd872e4430d7a9ce
Admin <kirillchistov@MacBook-Pro-Admin-2.local>
2026-01-17 01:33:43 +0300

    Fix color funcion

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,7 +20,7 @@ module.exports = {
     'no-restricted-exports': 'off',
     'no-param-reassign': 'off',
     'import/no-absolute-path': 'off',
-    'color-function-notation': 'legacy',
+    'color-function-notation': ['warn', 'legacy'],
     'import/prefer-default-export': 'off',
     'import/no-extraneous-dependencies': 'off',
     'max-len': 'off',
